785d6b325ab8b078f309d46fcbf8a1324a505dc0
LyndaATT <59966456+LyndaATT@users.noreply.github.com>
2020-01-22 15:54:07 +0100

    Add files via upload

---
+++ Frogger version1/src/frog/Frog.java
@@ -0,0 +1,40 @@
+package frog;
+
+import gameCommons.*;
+import util.Case;
+import util.Direction;
+
+public class Frog implements IFrog {
+
+	private Direction direction;
+	private Case position;
+	private Game game;
+
+	public Frog(Game game){
+		this.position = new Case(game.width/2, 0);
+		this.direction = Direction.up;
+		this.game = game;
+	}
+
+	public Case getPosition(){
+		return this.position;
+	}
+
+	public Direction getDirection(){
+		return this.direction;
+	}
+
+	public void move(Direction key){
+		this.direction = key;
+		switch(key){
+			case up   : if(this.position.ord!=game.height){ this.position = new Case(this.position.absc,this.position.ord+1);} break;
+			case down : if(this.position.ord!=0){ this.position = new Case(this.position.absc,this.position.ord-1);} break;
+			case left : if(this.position.absc!=0){ this.position = new Case(this.position.absc-1,this.position.ord);} break;
+			case right : if(this.position.absc!=game.width){ this.position = new Case(this.position.absc+1,this.position.ord);} break;
+		}
+		this.game.testWin();
+		this.game.testLose();
+		this.game.update();
+	}
+
+}
